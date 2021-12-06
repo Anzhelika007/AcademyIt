713604bd45483bd09009f16240a1504715275b18
Anzhelika <293852647@mail.ru>
2021-12-06 23:12:11 +0300

    broke the directory section

--- js/main.js
+++ js/main.js
@@ -9,5 +9,21 @@ $(function () {
         nextArrow: '<button class="header__slaider-btn header__slaider-btnnext"><img src="images/icon-bottom-slaider.svg"></button>',
     });
 
+    $('.search__tabs-item').on('click', function (e) {
+        e.preventDefault();
+
+        $('.tab').removeClass('tab--active');
+        $('.tabs-content').removeClass('tabs-content--active');
+
+        $(this).addClass('tab--active');
+        $($(this).attr('href')).addClass('tabs-content--active')
+    });
+
+
+    $('.product-item__favorite').on('click', function () {
+        $('.product-item__favorite').toggleClass('product-item__favorite--active')
+    });
+
+    $('.product-slider').slick({});
 
 });
