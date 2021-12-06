fc180f97eef95c5950ab2b738f6f14abae767389
Anzhelika <293852647@mail.ru>
2021-12-06 21:01:35 +0300

    add slider first page

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(function () {
+
+    $('.header__slaider').slick({
+        autoplay: true,
+        autoplaySpeed: 3000,
+        infinite: true,
+        dots: true,
+        prevArrow: '<button class="header__slaider-btn header__slaider-btnprev"><img src="images/icon-top-slaider.svg"></button>',
+        nextArrow: '<button class="header__slaider-btn header__slaider-btnnext"><img src="images/icon-bottom-slaider.svg"></button>',
+    });
+
+
+});
