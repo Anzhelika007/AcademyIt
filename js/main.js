bf642496cd66711fac42d80e279b6334b1830b1c
Anzhelika <293852647@mail.ru>
2021-12-14 20:01:43 +0300

    fixed bages

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 $(function () {
 
     $('.header__slaider').slick({
-        autoplay: true,
+        autoplay: false,
         autoplaySpeed: 2000,
         infinite: true,
         dots: true,
+        adaptiveHeight: true,
         prevArrow: '<button class="header__slaider-btn header__slaider-btnprev"><img src="images/icon-top-slaider.svg"></button>',
         nextArrow: '<button class="header__slaider-btn header__slaider-btnnext"><img src="images/icon-bottom-slaider.svg"></button>',
     });
