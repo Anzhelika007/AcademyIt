33ef84d98dca1e8c6f63873e2562c838df4ac9a6
Anzhelika <293852647@mail.ru>
2021-12-15 15:32:15 +0300

    add picture

--- js/main.js
+++ js/main.js
@@ -1,17 +1,15 @@
 $(function () {
 
-    $('.header__slaider').slick({
+    /* $('.header__slaider').slick({
         autoplay: false,
         autoplaySpeed: 2000,
         infinite: true,
         dots: true,
-        /* adaptiveHeight: true, */
         variableWidth: true,
         centerMode: true,
-        /*  raspondTo: window, */
         prevArrow: '<button class="header__slaider-btn header__slaider-btnprev"><img src="images/icon-top-slaider.svg"></button>',
         nextArrow: '<button class="header__slaider-btn header__slaider-btnnext"><img src="images/icon-bottom-slaider.svg"></button>',
-    });
+    }); */
 
 
     $('.brands__gallery').slick({
