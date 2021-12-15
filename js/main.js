9b34c85f190c28879e907ded84ba6b1228080823
Anzhelika <293852647@mail.ru>
2021-12-15 12:12:18 +0300

    page contacts done

--- js/main.js
+++ js/main.js
@@ -5,7 +5,10 @@ $(function () {
         autoplaySpeed: 2000,
         infinite: true,
         dots: true,
-        adaptiveHeight: true,
+        /* adaptiveHeight: true, */
+        variableWidth: true,
+        centerMode: true,
+        /*  raspondTo: window, */
         prevArrow: '<button class="header__slaider-btn header__slaider-btnprev"><img src="images/icon-top-slaider.svg"></button>',
         nextArrow: '<button class="header__slaider-btn header__slaider-btnnext"><img src="images/icon-bottom-slaider.svg"></button>',
     });
@@ -21,9 +24,34 @@ $(function () {
         nextArrow: '<button class="brands__gallery-btn brands__gallery-btnnext"><img src="images/icon-brand-right.png"></button>',
         responsive: [
             {
-                breakpoint: 1000,
+                breakpoint: 1330,
+                settings: {
+                    slidesToShow: 4
+                }
+            },
+            {
+                breakpoint: 1230,
                 settings: {
                     slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 950,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 620,
+                settings: {
+                    arrows: false,
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 470,
+                settings: {
+                    slidesToShow: 1
                 }
             }
         ]
@@ -50,6 +78,27 @@ $(function () {
         rows: 1,
         prevArrow: '<button class="reviers__slider-btn reviers__slider-btnprev"><img src="images/icon-brand-left.png"></button>',
         nextArrow: '<button class="reviers__slider-btn reviers__slider-btnnext"><img src="images/icon-brand-right.png"></button>',
+        responsive: [
+            {
+                breakpoint: 950,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 635,
+                settings: {
+                    slidesToShow: 1
+                }
+            },
+            {
+                breakpoint: 450,
+                settings: {
+                    arrows: false,
+                    slidesToShow: 1
+                }
+            }
+        ]
     });
 
 
